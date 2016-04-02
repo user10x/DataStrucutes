8f900dce400a0fe9b9811c42ec9fd576b3bc6279
nickhalden <ravichawla@ravis-MacBook-Pro.local>
2016-04-02 16:36:15 -0500

    PostOrder and PreOrder
    Bottom Up create tree node and Traverse the nodes

--- Tree.py
+++ Tree.py
@@ -1,1 +1,51 @@
 __author__ = 'nipunchawla'
+
+class Tree:
+
+    def __init__(self,cargo,left=None, right=None):
+        self.cargo=cargo
+        self.left=left
+        self.right=right
+
+    def __str__(self):
+        return self.cargo
+
+
+
+
+def total(tree):
+    #sum the tree nodes
+    if tree == None: return 0
+    return total(tree.left) + total(tree.right) + tree.cargo
+
+def printTree(tree,order):
+    if order=="pre":
+        if tree==None: return
+        print tree.cargo,
+        printTree(tree.left,order)
+        printTree(tree.right,order)
+    if order=="post":
+        if tree==None:return
+        printTree(tree.left,order)
+        printTree(tree.right,order)
+        print tree.cargo,
+
+
+
+
+
+if __name__ == '__main__':
+    left = Tree(2)
+    right = Tree(3)
+    root= Tree(1,left,right)
+    ret=total(root)
+    print ret
+    tree = Tree('+', Tree(1), Tree('*', Tree(2), Tree(3)))
+    printTree(tree,"pre")
+    print
+    printTree(tree,"post")
+
+
+
+
+
